922c396fa9e090514b37905f464b9c2760b827f2
Meho Mustafić <meho.mustafic@outlook.com>
2021-09-15 12:29:05 +0200

    Comitting Exercise 02

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    // Only change code below this line
+
+    // Comment using the first method
+    
+    /* Comment using the second method
+    with the stars*/
+
+
+    // Only change code above this line
+
+
+}
+
+console.log(main());
+
+module.exports = main;
